013f583684d1e70cf058c0c53f6dc9adff8174f6
Alex Miller <alexmillerprojects@gmail.com>
2020-02-28 08:21:58 -0800

    added form

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import PrivateRoute from './utils/PrivateRoute';
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <nav>
+          <div className='links'>
+          <Link to='/'>Login</Link>
+          <Link to='/bubbles'>Bubbles</Link>
+          </div>
+        </nav>
         <Route exact path="/" component={Login} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <PrivateRoute path='/bubbles' component={BubblePage} />
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-const Login = () => {
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [login, setLogin] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth().post('/api/login').then(res => {
+      console.log(res)
+      localStorage.setItem('token', res.data.payload)
+    })
+    .catch(err => console.log(err))
+  }
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+        <label>Username: <input type='text' name='username'></input></label>
+        <label>Password: <input type='password' name='password'></input></label><br/>
+        <button>Log in</button>
+      </form>
     </>
   );
 };
